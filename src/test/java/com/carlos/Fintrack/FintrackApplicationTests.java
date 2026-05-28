9c2dcfd7875d52d6ed93e4294b7f1390b2e212f1
Carlos Dias <carlosriquecorsi@gmail.com>
2026-05-28 20:04:47 -0300

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/carlos/Fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.Fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
